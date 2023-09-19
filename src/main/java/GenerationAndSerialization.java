1bfd31fcbd39874ca5b86332f11a12ab0f757e35
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2023-09-19 11:21:17 +0300

    some fix

--- src/main/java/GenerationAndSerialization.java
+++ src/main/java/GenerationAndSerialization.java
@@ -6,7 +6,7 @@ public class GenerationAndSerialization {
     public static void main (String args[]) throws IOException {
         List<Client> clients = Generator.generateObjects();
 
-        String filePath ="JSON/clients.json";
+        String filePath ="clients.json";
 
         Serialization<Client> serialization = new Serialization<>();
         serialization.serializeObjectsToJson(clients, filePath);
